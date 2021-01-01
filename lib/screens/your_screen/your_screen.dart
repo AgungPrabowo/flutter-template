85f993e61237ac521fedc44e3e82e0f2d111a007
AgungPrabowo <agungprabowo112@gmail.com>
2021-01-02 02:29:58 +0700

    added screens folder

---
+++ lib/screens/your_screen/your_screen.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class YourScreen extends StatefulWidget {
+  @override
+  _YourScreenState createState() => _YourScreenState();
+}
+
+class _YourScreenState extends State<YourScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
