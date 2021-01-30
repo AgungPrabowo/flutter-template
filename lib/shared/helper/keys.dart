d3767fe8c9e4e532ebad3d8d4643141c5a5dc937
Agung Prabowo <agungprabowo112@gmail.com>
2021-01-30 15:01:48 +0700

    Update keys.dart

--- lib/shared/helper/keys.dart
+++ lib/shared/helper/keys.dart
@@ -1,5 +1,6 @@
 class KeysGambling {
   // API
-  static const String BASE_API_URL = 'https://yourdomain.com/api/';
-  static const String BASE_URL = 'https://yourdomain.com/';
+  static const String BASE_URL = 'http://192.168.88.254/';
+  static const String BASE_API_URL = '${BASE_URL}api/';
+  static const String ASSET_URL = '${BASE_URL}public/storage/';
 }
