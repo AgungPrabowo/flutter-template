647791ff592f53077bb2141a6d2077d82b8a1be4
AgungPrabowo <agungprabowo112@gmail.com>
2021-07-23 23:32:20 +0700

    migrate null safety

--- lib/model/response_api.dart
+++ lib/model/response_api.dart
@@ -8,26 +8,22 @@ String responseApiToJson(ResponseApi data) => json.encode(data.toJson());
 class ResponseApi {
   ResponseApi({
     this.code,
-    this.error,
     this.message,
     this.data,
   });
 
-  int code;
-  dynamic error;
-  String message;
-  String data;
+  int? code;
+  String? message;
+  String? data;
 
   factory ResponseApi.fromJson(Map<String, dynamic> map) => ResponseApi(
         code: map["code"] ?? 0,
-        error: map["error"],
         message: map["message"] ?? "",
-        data: json.encode(map["data"]) ?? "",
+        data: map["data"] == null ? "" : json.encode(map["data"]),
       );
 
   Map<String, dynamic> toJson() => {
         "code": code ?? 0,
-        "error": error,
         "message": message ?? "",
         "data": data ?? "",
       };
--- lib/shared/helper/keys.dart
+++ lib/shared/helper/keys.dart
@@ -1,4 +1,4 @@
-class KeysGambling {
+class Keys {
   // API
   static const String BASE_URL = 'http://192.168.88.254/';
   static const String BASE_API_URL = '${BASE_URL}api/';
